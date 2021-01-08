0ecf2f52902385099cd6cec3f84c797d82ae13ce
Francisco de Assis <franciscoassisosilvaetb@gmail.com>
2021-01-08 14:26:38 -0300

    rotas crias para o controlador produtos e suas actios

--- Mvc_Rotas/App_Start/RouteConfig.cs
+++ Mvc_Rotas/App_Start/RouteConfig.cs
@@ -14,9 +14,28 @@ namespace Mvc_Rotas
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Produtos",
+                url: "Produtos/",
+                defaults: new { controller = "Produto", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Detalhes",
+                url: "Produtos/{produtoId}",
+                defaults: new { controller = "Produto", action = "Detalhes" },
+                constraints: new { produtoId = @"\d+" }
+            );
+
+            routes.MapRoute(
+                name: "Categorias",
+                url: "Produtos/{categoria}",
+                defaults: new { controller = "Produto", action = "Categorias" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Mvc_Rotas/Controllers/ProdutoController.cs
@@ -0,0 +1,37 @@
+﻿using Mvc_Rotas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Rotas.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private List<Produto> _listaProdutos;
+
+        public ProdutoController()
+        {
+            _listaProdutos = Produto.ListarProdutos().OrderBy(p=>p.Preco).ToList();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.TituloProdutos = "Todos os Produtos";
+            return View(_listaProdutos);
+        }
+
+        public ActionResult Detalhes(int produtoId)
+        {
+            return View(_listaProdutos.FirstOrDefault(p => p.ProdutoId == produtoId));
+        }
+
+        public ActionResult Categorias(string categoria)
+        {
+            ViewBag.Categoria = categoria;
+            List<Produto> produtos = _listaProdutos.Where(p => p.Categoria.Equals(categoria)).ToList();
+            return View(produtos);
+        }
+	}
+}
